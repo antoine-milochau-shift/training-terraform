5bed3dccd142ef59bcde4580ee682db769fa12b8
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-12-04 10:54:01 +0100

    Use random names

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
+resource "random_string" "name_suffix" {
+  length  = 16
+  special = false
+  upper   = false
+}
+
 resource "azurerm_resource_group" "resource_group" {
-  name     = "test-resource-group"
+  name     = "poc-terraform-${random_string.name_suffix.result}"
   location = "France Central"
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "teststorageaccount"
+  name                     = "poc${random_string.name_suffix.result}"
   location                 = "France Central"
   resource_group_name      = azurerm_resource_group.resource_group.name
   account_tier             = "Standard"
