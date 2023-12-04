c7ea9ca9923a8471ea62b56831e8f25f556f38db
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-12-04 11:54:33 +0100

    Remove `moved` blocks

--- main.tf
+++ main.tf
@@ -14,13 +14,3 @@ module "storage_account" {
   name_suffix         = random_string.name_suffix.result
   resource_group_name = module.resource_group.resource_group_name
 }
-
-moved {
-  from = azurerm_resource_group.resource_group
-  to   = module.resource_group.azurerm_resource_group.resource_group
-}
-
-moved {
-  from = azurerm_storage_account.storage_account
-  to   = module.storage_account.azurerm_storage_account.storage_account
-}
