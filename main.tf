0b696ca1d5f46c340a4b0e37aad36581f0203e83
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-12-04 11:09:26 +0100

    Make storage account compliant

--- main.tf
+++ main.tf
@@ -15,4 +15,12 @@ resource "azurerm_storage_account" "storage_account" {
   resource_group_name      = azurerm_resource_group.resource_group.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  cross_tenant_replication_enabled = false
+  enable_https_traffic_only        = true
+  min_tls_version                  = "TLS1_2"
+  allow_nested_items_to_be_public  = false
+  shared_access_key_enabled        = false
+  default_to_oauth_authentication  = true
+  allowed_copy_scope               = "AAD"
 }
